d0f067fb85983838c08e4e9785aa7032502be60f
Timothy Risi <timothy.risi@xamarin.com>
2016-01-27 23:06:40 -0900

    Put back iOS folder

---
+++ iOS/ClipboardService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency (typeof (SquadBuilder.iOS.ClipboardService))]
+namespace SquadBuilder.iOS
+{
+	public class ClipboardService : IClipboardService
+	{
+		public void CopyToClipboard(String text)
+		{
+			UIPasteboard clipboard = UIPasteboard.General;
+			clipboard.String = text;
+		}
+	}
+}
+
